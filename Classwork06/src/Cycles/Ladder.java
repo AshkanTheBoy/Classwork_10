868ef1874067454337fb49d11f2ac770fee6b154
AshkanTheBoy <bedrok@bk.ru>
2023-12-04 21:30:27 +0300

    Classwork

--- Classwork06/src/Cycles/Ladder.java
+++ Classwork06/src/Cycles/Ladder.java
@@ -2,6 +2,11 @@ package Cycles;
 
 public class Ladder {
     public static void main(String[] args) {
-
+        for (int i=0;i<9;i++) {
+            if (i==4) {
+                break;
+            }
+            System.out.println(i+1);
+        }
     }
 }
